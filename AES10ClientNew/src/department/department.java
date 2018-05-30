0e8da10c4963f05fa3f9d632fe202d81dc636f4c
Ron Yalensky <ron.yalensky@gmail.com>
2018-05-30 18:54:36 +0300

    #4

--- AES10ClientNew/src/department/department.java
+++ AES10ClientNew/src/department/department.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import Student.student;
 import lecturer.lecturer;
-// aviv first change #2
+// aviv first change #4
 /**
  * 
  *	Entity Class Contain department Details 
