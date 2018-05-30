dd991c90aec8d96c51b1aa545bf328cc3f2bcf33
Ron Yalensky <ron.yalensky@gmail.com>
2018-05-30 19:31:32 +0300

    fresh

--- AES10ClientNew/src/department/department.java
+++ AES10ClientNew/src/department/department.java
@@ -4,20 +4,11 @@ import java.util.ArrayList;
 
 import Student.student;
 import lecturer.lecturer;
-// aviv first change #5
 /**
  * 
  *	Entity Class Contain department Details 
  */
-<<<<<<< HEAD
-// rons first push
-=======
-public class departmenfsdgsdt  {
-	
-	private int numer=12;
-	public String name = "Roni";
-}
->>>>>>> refs/remotes/origin/master
+
 
 public class department implements Serializable {
 	private int did;
